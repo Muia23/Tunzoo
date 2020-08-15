e1bfaa1e5a22321b2d85087737632313de4f4e2b
Muia23 <wilfred.muema@gmail.com>
2020-08-15 13:20:30 +0300

    add bootstrap and base template file

--- tunzoo/settings.py
+++ tunzoo/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'projects.apps.ProjectsConfig',
+    'projects',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
