7c61f17b2f75d8829d715d559ff73b02401f8a72
Muia23 <wilfred.muema@gmail.com>
2020-08-17 08:41:23 +0300

    add first post to database and both template tags and interpolation in home template to display projects posted

--- projects/models.py
+++ projects/models.py
@@ -6,3 +6,11 @@ class Post(models.Model):
     url = models.CharField(max_length=120)    
     description = models.TextField()    
     screenshot = models.ImageField(upload_to = 'screenshot')
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def get_posts(cls):
+        posts = cls.objects.order_by('name')
+        return posts
--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Home page.
 def home(request):
     current_user = request.user
-    return render(request, 'index.html', {'current_user': current_user })
+    posts = Post.get_posts()
+    
+    return render(request, 'index.html', {"current_user": current_user, "posts": posts })
 
 # Detials page.
 @login_required(login_url='/accounts/login/')
